63e6bf8e7409c148d572654d68182cf4f1f8f4ba
Connor McCarthy <connor.t.k@icloud.com>
2025-12-29 16:53:54 +0000

    Implemented Product test class

--- src/main/java/ci553/happyshop/tests/cusModelTest.java
+++ src/main/java/ci553/happyshop/tests/cusModelTest.java
@@ -11,15 +11,6 @@ import static org.junit.Assert.assertTrue;
 
 public class cusModelTest {
 
-    // test product is built correctly
-    @Test
-    public void addProductTest() throws Exception {
-        Product p = new Product("0009", "ProductTest", "imageHolder.jpg", 2.00, 200);
-        assertEquals("0009", p.getProductId());
-        assertEquals("ProductTest", p.getProductDescription());
-        assertEquals("imageHolder.jpg", p.getProductImageName());
-        assertEquals(200, p.getStockQuantity());
-    }
 
     @Test
     public void checkOutTest() throws Exception {
