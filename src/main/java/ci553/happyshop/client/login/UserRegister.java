1ca17028de2765400ddd5ea718f88e9bf3d7c06e
Connor McCarthy <connor.t.k@icloud.com>
2026-01-15 23:47:08 +0000

    Branch ver 0.4.5 - User register will now correctly write each user to a new line

--- src/main/java/ci553/happyshop/client/login/UserRegister.java
+++ src/main/java/ci553/happyshop/client/login/UserRegister.java
@@ -17,7 +17,7 @@ public class UserRegister { ;
     public void createUser(TextField username, TextField password, String userType) throws IOException {
         // set filepath, write parameters to txt files with a new line
         FileWriter fw = new FileWriter(filePath, true);
-        fw.write(username.getText() + "," + password.getText() + "," + userType);
+        fw.write(username.getText() + "," + password.getText() + "," + userType + System.lineSeparator());
         fw.close();
     }
 
