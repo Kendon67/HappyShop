30e398f99bb4a5eb1310b18c8d92e127c6a28e7b
Connor McCarthy <connor.t.k@icloud.com>
2026-01-15 23:32:20 +0000

    Branch ver 0.3.2 - Fixed trolley UI being pushed too far to the right

--- src/main/java/ci553/happyshop/client/customer/CustomerView.java
+++ src/main/java/ci553/happyshop/client/customer/CustomerView.java
@@ -133,7 +133,7 @@ public class CustomerView  {
         hbSearchResult.setAlignment(Pos.CENTER_LEFT);
 
         VBox vbSearchPage = new VBox(15, laPageTitle, hbId, hbName, hbBtns, hbSearchResult);
-        vbSearchPage.setPrefWidth(UIStyle.customerWinWidth);
+        vbSearchPage.setPrefWidth(COLUMN_WIDTH);
         vbSearchPage.setAlignment(Pos.TOP_CENTER);
         vbSearchPage.setStyle("-fx-padding: 15px;");
 
